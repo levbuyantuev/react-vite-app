48a1801cd8c777e5b65f4e5638f0f507acf68b69
Lev_Buyantuev <levbuyantuev2019@gmail.com>
2026-03-01 08:39:13 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "/src"),
     },
   },
 });
